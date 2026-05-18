a20ba93f2972ec91d79ae4acbb3c555a87d640f8
Zheng Ma <mazhengcn@outlook.com>
2026-05-18 20:51:51 +0800

    remove explicit colorscheme command; integrate colorscheme option into LazyVim configuration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,7 +10,12 @@ return {
 					transparent = true,
 				},
 			})
-			vim.cmd("colorscheme github_dark_default")
 		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "github_dark_default",
+		},
 	},
 }
