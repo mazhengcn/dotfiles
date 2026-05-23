6bd28c713a3ab64f2d6e32938f18259d0859feb1
Zheng Ma <mazhengcn@outlook.com>
2026-05-23 13:39:37 +0800

    add github dark theme for nvim and tmux back

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -13,9 +13,19 @@ return {
     },
   },
   {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    transparent = true,
+    config = function()
+      require("github-theme").setup({})
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "github_dark_default",
     },
   },
 }
