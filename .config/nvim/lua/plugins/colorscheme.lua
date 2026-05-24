9981209c7534e575bbbc6d584b146e99a27b0303
Zheng Ma <mazhengcn@outlook.com>
2026-05-24 16:09:22 +0800

    update colortheme for nvim and tmux

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -15,17 +15,13 @@ return {
   {
     "projekt0n/github-nvim-theme",
     name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = true, -- make sure we load this during startup if it is your main colorscheme
     transparent = true,
-    config = function()
-      require("github-theme").setup({})
-    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_default",
+      colorscheme = "tokyonight",
     },
   },
 }
