dbabacf02a123b286189385cd59e8c4245841a09
Zheng Ma <mazhengcn@outlook.com>
2026-06-05 09:36:49 +0800

    update nvim options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,7 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
-opt.smoothscroll = false
-opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamedplus"
